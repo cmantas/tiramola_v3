a6a1db70cbacf85f0338f893c4171edd981c08d8
Christos Mantas <cmantas@cslab.ece.ntua.gr>
2015-01-28 16:47:25 +0200

    sync

--- new_decision_module.py
+++ new_decision_module.py
@@ -212,18 +212,17 @@ class RLDecisionMaker:
 
         return ctd
 
-
-def moving_average(self, iterable, n=3):
-    # moving_average([40, 30, 50, 46, 39, 44]) --> 40.0 42.0 45.0 43.0
-    # http://en.wikipedia.org/wiki/Moving_average
-    it = iter(iterable)
-    d = deque(itertools.islice(it, n - 1))
-    d.appendleft(0)
-    s = sum(d)
-    for elem in it:
-        s += elem - d.popleft()
-        d.append(elem)
-        yield s / float(n)
+    def moving_average(self, iterable, n=3):
+        # moving_average([40, 30, 50, 46, 39, 44]) --> 40.0 42.0 45.0 43.0
+        # http://en.wikipedia.org/wiki/Moving_average
+        it = iter(iterable)
+        d = deque(itertools.islice(it, n - 1))
+        d.appendleft(0)
+        s = sum(d)
+        for elem in it:
+            s += elem - d.popleft()
+            d.append(elem)
+            yield s / float(n)
 
     def predict_load(self):
         # Linear Regression gia na doume to slope
